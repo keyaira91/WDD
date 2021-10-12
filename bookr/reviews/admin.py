ee9b021a88da314b449bd00de19d950715390c02
keyaira91 <keyaira22@hotmail.com>
2021-10-12 13:25:41 -0400

    activity 4.02

--- bookr/reviews/admin.py
+++ bookr/reviews/admin.py
@@ -14,11 +14,15 @@ class ReviewAdmin(admin.ModelAdmin):
     fieldsets = ((None, {'fields': ('creator', 'book')}), \
         ('Review content', {'fields': ('content', 'rating')}))
 
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = ('last_names', 'first_names')
+    search_fields = ('last_names__startswith', 'first_names',)
+    list_filter = ('last_names',)
 
 # Register your models here.
 
 admin.site.register(Publisher)
-admin.site.register(Contributor)
+admin.site.register(Contributor, ContributorAdmin)
 admin.site.register(Book, BookAdmin)
 admin.site.register(BookContributor)
 admin.site.register(Review, ReviewAdmin)
--- bookr/reviews/models.py
+++ bookr/reviews/models.py
@@ -30,9 +30,13 @@ class Contributor(models.Model):
     first_names = models.CharField(max_length=50, help_text="The contributor's first name or names.")
     last_names = models.CharField(max_length=50, help_text="The contributor's last name or names.")
     email = models.EmailField(help_text="The contact email for the contributor.")
-    def __str__(self):
-        return self.first_names
 
+    def initialled_name(self):
+        initials = ''.join([name[0] for name in self.first_names.split(' ')])
+        return f"{initials}"
+
+    def __str__(self):
+        return self.last_names, self.initialled_name()
 class BookContributor(models.Model):
     class ContributionRole(models.TextChoices):
         AUTHOR = "AUTHOR", "Author"
